2b239d85b77aef13d9c379d567d0a57d2db439cf
gabino garcia <gabinogarciavalencia1108@gmail.com>
2021-12-21 01:04:26 -0600

    proyecto

--- WebApp/model/frmProductos.py
+++ WebApp/model/frmProductos.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.fields.choices import SelectField
 from wtforms.validators import DataRequired
 class FrmProductos(FlaskForm):
     descripcion = StringField("Descripción:",validators=[DataRequired()])
--- WebApp/model/frmProveedores.py
+++ WebApp/model/frmProveedores.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.fields.choices import SelectField
 from wtforms.validators import DataRequired
 class FrmProveedores(FlaskForm):
     proveedor = StringField("Agregar proveedor:",validators=[DataRequired()])
